29f23f0361dc58f7d44fbe61836e4c5c5cdaccd5
Punna00 <42738109+Punna00@users.noreply.github.com>
2020-07-16 18:14:51 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,4 @@
-const functions = require('firebase-functions');
 
-const app = require('express')();
-
-const FBAuth = require('./util/fbAuth');
-
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login } = require('./handlers/users');
-
-// screams routes
-app.get('/screams', getAllScreams);
-app.post('/scream', FBAuth, postOneScream);
-
-// users routes
-app.post('/signup', signup);
-app.post('/login', login);
-
-exports.api = functions.https.onRequest(app);
 
 
 
