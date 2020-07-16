c90d2d99e023691ae05b4b70a41f4676eb700194
Punna00 <poko-nyan@hotmail.com>
2020-07-16 15:05:44 +0700

    DB Schema

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-07-13T08:12:58.017Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
